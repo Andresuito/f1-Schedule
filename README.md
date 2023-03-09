3e95d4557c3931e45cc6a58dcd414f4480335138
Andresuito <barrancorios@gmail.com>
2023-03-09 13:16:01 +0100

    - Añadida una fuente del estilo F1 - Mejorado un poco el responsive - Ahora se muestran ordenados los GP - Cambiado un poco el estilo de la caja

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # F1 Schedule
 
 ### Framework
+
 - [Bootstrap](https://blog.getbootstrap.com/)
 
 ### Api
+
 - [Ergast Developer API](http://ergast.com/mrd/)
 
+### Font
+
+- [F1 Font](https://webfonts.ffonts.net/Formula1-Display-Bold-Bold.font)
+
 ### Image
+
 ![alt text](https://i.imgur.com/upzaWAJ.png)
 
 ### Features
+
 - More attractive design
 - Different colors
 - I will add images
-
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const getLocalTime = (utcDateStr) => {
   const localTime = new Date(utcDate.getTime());
 
   const options = {
-    timeZone: "Europe/Madrid", // Cambia a la zona horaria adecuada
+    timeZone: "Europe/Madrid",
     hour12: false,
     hour: "2-digit",
     minute: "2-digit",
@@ -13,21 +13,20 @@ const getLocalTime = (utcDateStr) => {
   return localTime.toLocaleString("es-ES", options);
 };
 
-const getRaceTime = (raceId) => {
+const getRaceTime = async (raceId) => {
   const raceUrl = `https://ergast.com/api/f1/2023/${raceId}.json`;
 
-  return fetch(raceUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const raceTimeStr =
-        data.MRData.RaceTable.Races[0].date +
-        "T" +
-        data.MRData.RaceTable.Races[0].time;
-      return getLocalTime(raceTimeStr);
-    })
-    .catch((error) => {
-      console.error("Error al obtener la hora de la carrera:", error);
-    });
+  try {
+    const response = await fetch(raceUrl);
+    const data = await response.json();
+    const raceTimeStr =
+      data.MRData.RaceTable.Races[0].date +
+      "T" +
+      data.MRData.RaceTable.Races[0].time;
+    return getLocalTime(raceTimeStr);
+  } catch (error) {
+    console.error("Error al obtener la hora de la carrera:", error);
+  }
 };
 
 const isRaceWeekend = (startDate, endDate) => {
@@ -35,89 +34,106 @@ const isRaceWeekend = (startDate, endDate) => {
   return now >= startDate && now <= endDate;
 };
 
+const sortRaces = (races) => {
+  return races.sort((a, b) => {
+    const aDate = new Date(a.date);
+    const bDate = new Date(b.date);
+    return aDate - bDate;
+  });
+};
+
 fetch(apiUrl)
   .then((response) => response.json())
-  .then((data) => {
-    const races = data.MRData.RaceTable.Races;
+  .then(async (data) => {
+    const races = sortRaces(data.MRData.RaceTable.Races);
     const calendar = document.getElementById("calendar");
 
-    races.forEach(async (race) => {
+    for (const race of races) {
       const card = document.createElement("div");
       card.classList.add("col");
 
-      const raceTime = await getRaceTime(race.round);
       const startDate = new Date(race.date);
       const endDate = new Date(race.date);
-      endDate.setDate(endDate.getDate() + 2); // cada GP dura 3 días, así que agregamos 2 días al inicio
+      endDate.setDate(endDate.getDate() + 2);
 
-      card.classList.add("col-md-4", "mb-4");
+      card.classList.add("col-lg-4", "col-md-6", "mb-4"); // Agregamos clase col-md-6 para pantallas medianas
       card.innerHTML = `
-      <div class="card f1-card shadow-lg rounded-lg">
-      <div class="card-header f1-card-header bg-danger text-white">
-        <div class="d-flex align-items-center justify-content-between">
-          <h3 class="m-0">${race.raceName}</h3>
-        </div>
-        <div class="d-flex align-items-center justify-content-between mt-2">
-          <p class="m-0">${race.Circuit.Location.locality}, ${
-        race.Circuit.Location.country
-      }</p>
-          <h5 class="m-0 bg-dark text-white d-flex align-items-center justify-content-center f1-date-header"><i class="fas fa-calendar-alt me-2"></i>${startDate
-            .toLocaleDateString("en-US", {
-              month: "short",
-              day: "numeric",
-              localeMatcher: "best fit",
-            })
-            .toUpperCase()} - ${endDate
+      <div class="card shadow-lg rounded-lg mx-auto">
+        <div class="card-header header bg-danger text-white">
+          <div class="d-flex align-items-center justify-content-between">
+      <h3 class="m-0">${race.raceName.replace("Grand Prix", "GP")}</h3>
+            </div>
+            <div
+            class="d-flex flex-column flex-md-row justify-content-between mt-2"
+          >            <p class="m-0 text-start mb-2 mb-md-0">${
+            race.Circuit.Location.locality
+          }, ${race.Circuit.Location.country}</p>
+      <h5
+      class="m-0 bg-black text-white d-flex justify-content-center text-center d-block text-md-start fs-6 ms-md-3"
+      >      <i class="fas fa-calendar-alt me-2"></i>
+                ${startDate
+                  .toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                    localeMatcher: "best fit",
+                  })
+                  .toUpperCase()} - ${endDate
         .toLocaleDateString("en-US", {
           month: "short",
           day: "numeric",
           localeMatcher: "best fit",
         })
-        .toUpperCase()}</h5>
-        </div>
-      </div>
-      <div class="card-body f1-card-body">
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">
-            <i class="fas fa-stopwatch me-2"></i><strong>Free Practice 1:</strong> ${
-              race.FirstPractice
-                ? getLocalTime(
-                    race.FirstPractice.date + "T" + race.FirstPractice.time
-                  )
-                : "TBA"
-            }
-          </li>
-          <li class="list-group-item">
-            <i class="fas fa-stopwatch me-2"></i><strong>Free Practice 2:</strong> ${
-              race.SecondPractice
-                ? getLocalTime(
-                    race.SecondPractice.date + "T" + race.SecondPractice.time
-                  )
-                : "TBA"
-            }
-          </li>
-          <li class="list-group-item">
-            <i class="fas fa-stopwatch me-2"></i><strong>Free Practice 3:</strong> ${
-              race.ThirdPractice
-                ? getLocalTime(
-                    race.ThirdPractice.date + "T" + race.ThirdPractice.time
-                  )
-                : "TBA"
-            }
-          </li>
-          <li class="list-group-item">
-            <i class="fas fa-stopwatch me-2"></i><strong>Qualifying:</strong> ${
-              race.ThirdPractice
-                ? getLocalTime(
-                    race.Qualifying.date + "T" + race.Qualifying.time
-                  )
-                : "TBA"
-            }
-          </li>
-              <li class="list-group-item">
-              <i class="fa-solid fa-flag-checkered me-2"></i><strong>Race:</strong> ${
-                  race.date ? getLocalTime(race.date + "T" + race.time) : "TBA"
+        .toUpperCase()}
+              </h5>
+            </div>
+          </div>
+          <div class="card-body">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item bg-lighter-gray text-black">
+                <i class="fas fa-stopwatch me-2"></i><strong>Free Practice 1 - </strong>
+                ${
+                  race.FirstPractice
+                    ? await getLocalTime(
+                        race.FirstPractice.date + "T" + race.FirstPractice.time
+                      )
+                    : "TBA"
                 }
+              </li>
+              <li class="list-group-item bg-lighter-gray text-black">
+                <i class="fas fa-stopwatch me-2"></i><strong>Free Practice 2 - </strong>
+                ${
+                  race.SecondPractice
+                    ? await getLocalTime(
+                        race.SecondPractice.date +
+                          "T" +
+                          race.SecondPractice.time
+                      )
+                    : "TBA"
+                }
+              </li>
+              <li class="list-group-item bg-lighter-gray text-black">
+                <i class="fas fa-stopwatch me-2"></i><strong>Free Practice 3 - </strong>
+                ${
+                  race.ThirdPractice
+                    ? await getLocalTime(
+                        race.ThirdPractice.date + "T" + race.ThirdPractice.time
+                      )
+                    : "TBA"
+                }
+          </li>
+              <li class="list-group-item bg-lighter-gray text-black">
+                <i class="fas fa-flag-checkered me-2"></i><strong>Qualifying - </strong>
+                ${
+                  race.Qualifying
+                    ? await getLocalTime(
+                        race.Qualifying.date + "T" + race.Qualifying.time
+                      )
+                    : "TBA"
+                }
+              </li>
+              <li class="list-group-item bg-lighter-gray text-black">
+                <i class="fas fa-flag-checkered me-2"></i><strong>Race - </strong>
+                ${race.raceTime ? await getLocalTime(race.raceTime) : "TBA"}
               </li>
             </ul>
           </div>
@@ -125,8 +141,10 @@ fetch(apiUrl)
       `;
 
       calendar.appendChild(card);
-    });
+    }
   })
   .catch((error) => {
-    console.error("Error al obtener el calendario:", error);
+    console.error("Error fetching data:", error);
   });
+
+/*TESTING */
